f17dac4a64563f6c9d19c7f7cafb0526e50d406d
julic20s <julic20s@outlook.com>
2025-03-16 17:42:22 +0800

    Add function expr ir

---
+++ src/execution/expression_ir/ir_function.cpp
@@ -0,0 +1,24 @@
+#include "duckdb/execution/expression_ir_generator.hpp"
+#include "duckdb/planner/expression/bound_function_expression.hpp"
+
+#include <algorithm>
+
+namespace duckdb {
+
+bool BoundFunctionExpression::IsCompilable() const {
+    if (function.ir_generate == nullptr) {
+        return false;
+    }
+	return std::all_of(children.begin(), children.end(), [](auto &child) { return child->IsCompilable(); });
+}
+
+llvm::Value *ExpressionIRGenerator::Generate(BoundFunctionExpression &expr) {
+	vector<llvm::Value *> arguments;
+	arguments.reserve(expr.children.size());
+	for (auto &child : expr.children) {
+		arguments.push_back(Generate(*child));
+	}
+	return expr.function.ir_generate(*rewriter, arguments);
+}
+
+} // namespace duckdb
